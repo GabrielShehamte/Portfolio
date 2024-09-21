baf43dde5dc6f0460ce6152d31b9c6be10926ed7
GabrielShehamte <Gabriel.shehamte@gmail.com>
2024-09-21 01:51:54 -0400

    Updates

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ st.markdown("""
 col1, col2, col3 = st.columns([1,2,1])
 
 with col1:
-     st.image("Image2.png", width=200)
+     st.image("Images/Image2.png", width=200)
     
 st.title("Homepage")
 st.subheader("Favourite Quote") # Replace the favourite quote with an actual quote.
@@ -38,15 +38,14 @@ st.markdown("---")
     st.write("""Professionally, I aspire to work for a major tech firm like Google, Microsoft, or Apple,
              while building the foundations for my own consulting company in the tech industry. 
              My ultimate goal is to achieve financial freedom, enabling me to travel the world, 
-             spend quality time with family and friends, and truly enjoy all that life has to offer.""")
-    st.write("""        
-
-I hold a degree in Management Engineering and am passionate about fitness,
-whether it's hitting the gym, playing basketball, or enjoying a game of football. 
-I love exploring new places through traveling, it's always been a dream of mine 
-to be able to see the world, and I aim to leverage my career successs to allow me 
-to do so.
-             
+             spend quality time with family and friends, and truly enjoy all that life has to offer.
+             """)
+   
+    st.write("""I hold a degree in Management Engineering and am passionate about fitness,
+            whether it's hitting the gym, playing basketball, or enjoying a game of football. 
+            I love exploring new places through traveling, it's always been a dream of mine 
+            to be able to see the world, and I aim to leverage my career successs to allow me 
+            to do so.         
              """)
     
     st.write("""
@@ -54,33 +53,34 @@ to do so.
              implementing games, python packages, and code one can implement into their company.
     """)
 
-with col3:
-    st.write("Contact Me")
+# with col3:
+#     st.write("Contact Me")
     
 
 
 cols = st.columns(4)
 
 tools = [
-    {"name":"Task Organizer" ,"button_text": "Link/Source1"},
-    {"name":"Portfolio Site" ,"button_text": "Link/Source2"},
-    {"name":"Games" ,"button_text": "Link/Source3"},
-    {"name":"PDF Generator" ,"button_text": "Link/Source4"}
+    {"name":"Task Organizer",  "imageArea": "Images/TaskOrganizer.png", "button_text": "Link/Source1"},
+    {"name":"Portfolio Site",  "imageArea": "Images/portfolioimage.png", "button_text": "Link/Source2"},
+    {"name":"Games",  "imageArea": "Images/Games.png", "button_text": "Link/Source3"},
+    {"name":"PDF Generator",  "imageArea": "Images/PDF_Generator_API.png", "button_text": "Link/Source4"}
 ]
 
 
 for i, tool in enumerate(tools):
     with cols[i]:
         st.write(f"### {tool['name']}")
+        st.image(f"{tool['imageArea']}")
         st.button(tool['button_text'])
         
         
 st.write("Projects i'm working on:")
 
 projects = [
-    {"name": "Project 1", "button_text": "Link/Source5"},
-    {"name": "Project 2", "button_text": "Link/Source6"},
-    {"name": "Project 3", "button_text": "Link/Source7"}
+    {"name": "NBA Predictor", "imageArea": "Images/NBA_Analytics.png","button_text": "Link/Source5"},
+    # {"name": "Project 2", "imageArea": "image6", "button_text": "Link/Source6"},
+    # {"name": "Project 3", "imageArea": "image7", "button_text": "Link/Source7"}
 ]
     
 proj_cols = st.columns(3)
@@ -88,6 +88,7 @@ proj_cols = st.columns(3)
 for i, project in enumerate(projects):
     with proj_cols[i]:
         st.write(f"{project['name']}")
+        st.image(f"{project['imageArea']}", width=200)
         st.button(project['button_text'])
         
         
